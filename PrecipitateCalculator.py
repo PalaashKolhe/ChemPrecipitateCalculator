77a8381c4590e03f79ea07ad48cd947990a06f9d
Palaash Kolhe <p.kolhe@share.epsb.ca>
2019-04-24 13:51:12 -0600

    first upload

---
+++ PrecipitateCalculator.py
@@ -0,0 +1,19 @@
+'''
+title: Precipitate Calculator
+author: Palaash Kolhe
+date created: 2018-04-24
+'''
+
+# Arrays
+pos = []
+neg = []
+
+# Inputs
+posIon = input("Please enter the positive reacting ion. (No Charges): ")
+posVol = input("What is the volume of the positive solution? (L): ")
+posConc = input("What is the concentration of the positive solution. (mol/L): ")
+
+negIon = input("Please enter the negative reacting ion. (No Charges): ")
+negVol = input("What is the volume of the negative solution? (L): ")
+negConc = input("What is the concentration of the negative solution? (mol/L): ")
+
